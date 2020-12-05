6d74ccdc6ff69f788ce1fd847b06f407227c2cb1
ThomsonTang <guiketang@gmail.com>
2020-12-05 10:24:26 +0800

    feat(readme): add a readme file.

--- image-utils/src/main/java/com/tt/tutorials/image/thumbnailator/ImageUtils.java
+++ image-utils/src/main/java/com/tt/tutorials/image/thumbnailator/ImageUtils.java
@@ -1,17 +1,16 @@
 package com.tt.tutorials.image.thumbnailator;
 
-import net.coobird.thumbnailator.filters.Caption;
-import net.coobird.thumbnailator.geometry.Position;
-import net.coobird.thumbnailator.geometry.Positions;
-
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-
 import javax.imageio.ImageIO;
+import net.coobird.thumbnailator.filters.Caption;
+import net.coobird.thumbnailator.geometry.Position;
+import net.coobird.thumbnailator.geometry.Positions;
 
 /**
  * 图片处理工具类
@@ -20,26 +19,26 @@ import javax.imageio.ImageIO;
  */
 public class ImageUtils {
 
-    public static void addCaption(String originalImagePath, String captionText) {
-        try {
-            BufferedImage originalImage = ImageIO.read(new File(originalImagePath));
+  public static void addCaption(String originalImagePath, String captionText) {
+    try {
+      BufferedImage originalImage = ImageIO.read(new File(originalImagePath));
 
-            Font font = new Font("Monaco", Font.PLAIN, 14);
-            Color color = Color.RED;
-            Position position = Positions.BOTTOM_RIGHT;
-            Caption caption = new Caption(captionText, font, color, position, 4);
-            BufferedImage targetImage = caption.apply(originalImage);
-            ImageIO.write(targetImage, "png", new File("/Users/thomsontang/temp/caption2.png"));
-            System.out.println("done");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+      Font font = new Font("Monaco", Font.PLAIN, 14);
+      Color color = Color.RED;
+      Position position = Positions.BOTTOM_RIGHT;
+      Caption caption = new Caption(captionText, font, color, position, 4);
+      BufferedImage targetImage = caption.apply(originalImage);
+      ImageIO.write(targetImage, "png", new File("/Users/thomsontang/temp/caption2.png"));
+      System.out.println("done");
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+  }
 
-    public static void main(String[] args) {
-        LocalDateTime now = LocalDateTime.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        String formatDateTime = now.format(formatter);
-        ImageUtils.addCaption("/Users/thomsontang/Pictures/annotate/kaoqing.png", formatDateTime);
-    }
+  public static void main(String[] args) {
+    LocalDateTime now = LocalDateTime.now();
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    String formatDateTime = now.format(formatter);
+    ImageUtils.addCaption("/Users/thomsontang/Pictures/annotate/kaoqing.png", formatDateTime);
+  }
 }
